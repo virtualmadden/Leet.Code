53f6d10ff47df9f814f2f43c73ba7e1fb1c32fe2
Jonathan Madden <virtualmadden@users.noreply.github.com>
2018-06-04 10:36:20 -0700

    solution

---
+++ Leet.Code.Solutions/Easy/FlippingAnImage.cs
@@ -0,0 +1,53 @@
+﻿namespace Leet.Code.Solutions.Easy
+{
+    public class FlippingAnImage
+    {
+        public int[][] FlipAndInvertImage(int[][] image)
+        {
+            var flippedImage = FlipImage(image);
+
+            return InvertImage(flippedImage);
+        }
+
+        private static int[][] FlipImage(int[][] image)
+        {
+            var flippedImage = new int[image.GetLength(0)][];
+
+            for (var i = 0; i < image.GetLength(0); i++)
+            {
+                flippedImage[i] = new int[image.GetLength(0)];
+
+                for (var j = 0; j < image.GetLength(0); j++)
+                {
+                    flippedImage[i][j] = image[i][image.GetLength(0) - j - 1];
+                }
+            }
+
+            return flippedImage;
+        }
+
+        private static int[][] InvertImage(int[][] image)
+        {
+            var invertedImage = new int[image.GetLength(0)][];
+
+            for (var i = 0; i < image.GetLength(0); i++)
+            {
+                invertedImage[i] = new int[image.GetLength(0)];
+
+                for (var j = 0; j < image.GetLength(0); j++)
+                {
+                    if (image[i][j].Equals(0))
+                    {
+                        invertedImage[i][j] = 1;
+                    }
+                    else
+                    {
+                        invertedImage[i][j] = 0;
+                    }
+                }
+            }
+
+            return invertedImage;
+        }
+    }
+}

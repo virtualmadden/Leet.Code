d8a8f6350cc589266a6b0e4ab4df16d96956a9e0
Jonathan Madden <virtualmadden@users.noreply.github.com>
2018-06-04 14:31:00 -0700

    solution

---
+++ Leet.Code.Solutions.Tests/Medium/KeepCitySkylineTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Leet.Code.Solutions.Medium;
+using NUnit.Framework;
+
+namespace Leet.Code.Solutions.Tests.Medium
+{
+    [TestFixture]
+    public class KeepCitySkylineTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _keepCitySkyline = new KeepCitySkyline();
+        }
+
+        private KeepCitySkyline _keepCitySkyline;
+
+        [Test]
+        public void DoSomething()
+        {
+            var cityGrid = new[]
+            {
+                new[] {3, 0, 8, 4},
+                new[] {2, 4, 5, 7},
+                new[] {9, 2, 6, 3},
+                new[] {0, 3, 1, 0}
+            };
+
+            _keepCitySkyline.MaxIncreaseKeepingSkyline(cityGrid).Should().Be(35);
+        }
+    }
+}

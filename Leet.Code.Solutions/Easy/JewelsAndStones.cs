e07a6b1fe203127a0c0d16939ed41c3bb7fb3765
Jonathan Madden <virtualmadden@users.noreply.github.com>
2018-06-04 11:12:12 -0700

    solution

---
+++ Leet.Code.Solutions/Easy/JewelsAndStones.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace Leet.Code.Solutions.Easy
+{
+    public class JewelsAndStones
+    {
+        public int NumJewelsInStones(string jewels, string stones)
+        {
+            var numOfJewels = 0;
+
+            foreach (var stone in stones)
+            {
+                if (jewels.Any(x => x.Equals(stone)))
+                {
+                    numOfJewels++;
+                }
+            }
+
+            return numOfJewels;
+        }
+    }
+}

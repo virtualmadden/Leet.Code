0fef2379b8cb84bbc6144a7c5536fbd9d0d65fb9
Jonathan Madden <virtualmadden@users.noreply.github.com>
2018-06-04 10:25:19 -0700

    initial commit

---
+++ Leet.Code.Solutions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Leet.Code.Solutions
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

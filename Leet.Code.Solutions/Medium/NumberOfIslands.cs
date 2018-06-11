ad3111afa5521bf7feaae10d576f327d468e41ef
Jonathan Madden <virtualmadden@users.noreply.github.com>
2018-06-11 14:21:20 -0700

    solution

---
+++ Leet.Code.Solutions/Medium/NumberOfIslands.cs
@@ -0,0 +1,27 @@
+﻿namespace Leet.Code.Solutions.Medium
+{
+    public class NumberOfIslandsSolution
+    {
+        public int NumIslands(char[,] grid)
+        {
+            var islands = 0;
+
+            for (var i = 0; i < grid.GetLength(0); i++)
+            {
+                for (var j = 0; j < grid.GetLength(1); j++)
+                {
+                    if (grid[i, j].Equals('1'))
+                    {
+                        islands++;
+                    }
+
+                    if (grid[i, j].Equals('0'))
+                    {
+                    }
+                }
+            }
+
+            return islands;
+        }
+    }
+}

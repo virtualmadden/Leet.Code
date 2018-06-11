1323e071204ea9f783617df0cd31a2fe7aeec327
Jonathan Madden <virtualmadden@users.noreply.github.com>
2018-06-11 14:55:39 -0700

    build fix

--- Leet.Code.Solutions.Tests/Easy/MaximumSubarrayTests.cs
+++ Leet.Code.Solutions.Tests/Easy/MaximumSubarrayTests.cs
@@ -15,14 +15,14 @@ namespace Leet.Code.Solutions.Tests.Easy
             _solution = new MaximumSubarraySolution();
         }
 
-        private readonly List<Tuple<int[], int>> _testCases = new List<Tuple<int[], int>>
+        private static readonly List<Tuple<int[], int>> TestCases = new List<Tuple<int[], int>>
         {
             new Tuple<int[], int>(new[] {-2, 1, -3, 4, -1, 2, 1, -5, 4}, 6)
         };
 
         private MaximumSubarraySolution _solution;
 
-        [TestCaseSource(nameof(_testCases))]
+        [TestCaseSource(nameof(TestCases))]
         public void ShouldReturnTheLargestSum(Tuple<int[], int> testCase)
         {
             _solution.MaxSubArray(testCase.Item1).Should().Be(testCase.Item2);

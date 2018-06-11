7b04f04aef93f34098014a0b9fbe39a2f4a3d7f1
Jonathan Madden <virtualmadden@users.noreply.github.com>
2018-06-11 14:08:46 -0700

    solution

---
+++ Leet.Code.Solutions.Tests/Easy/MaximumSubarrayTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Leet.Code.Solutions.Easy;
+using NUnit.Framework;
+
+namespace Leet.Code.Solutions.Tests.Easy
+{
+    [TestFixture]
+    public class MaximumSubarrayTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _solution = new MaximumSubarraySolution();
+        }
+
+        private MaximumSubarraySolution _solution;
+
+        [Test]
+        public void ShouldReturnTheLargestSum()
+        {
+            var array = new[] {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+            _solution.MaxSubArray(array).Should().Be(6);
+        }
+    }
+}
